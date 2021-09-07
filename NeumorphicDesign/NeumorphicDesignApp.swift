0a9a841071d30a1227191304bfb4861852f6bfc5
SmithZachary <zachary.6.smith@gmail.com>
2021-09-07 13:26:06 -0500

    Initial Commit

---
+++ NeumorphicDesign/NeumorphicDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeumorphicDesignApp.swift
+//  NeumorphicDesign
+//
+//  Created by Zach Smith on 9/7/21.
+//
+
+import SwiftUI
+
+@main
+struct NeumorphicDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
